3cd32978513ea83eadb48eea9e85ecdd890ca044
manish-kumar-2002 <manishkumar8923245598@gmail.com>
2024-08-12 05:40:54 +0530

    add today self talk 2

--- 11-08-2004/object.js
+++ 11-08-2004/object.js
@@ -56,5 +56,78 @@ jsUser.FullAddress = {
     }
 }
 
+
 console.log(jsUser.FullAddress.houseNo);
 console.log(jsUser.FullAddress.citizen.citizenBy);
+
+
+
+
+const obj1 = {
+    a : "1",
+    b : "2",
+    c : "3"
+}
+
+
+const obj2 = {
+    d : "4",
+    e : "5",
+    f : "6"
+}
+
+
+
+const obj3 = {obj1,obj2}
+const obj4 = {...obj1,...obj2}
+const obj5 = Object.assign({},obj1,obj2) // Object.assign(target,source) source ek se jada ho skte hai or abhi source add ho kr target me add ho jate hai (now return new object or target object same hai .....)
+
+console.log(obj3);
+console.log(obj4);
+console.log(obj5);
+
+
+
+
+const newUser = [
+    {
+        id : 1,
+        name : "aman"
+    },
+    {
+        id : 2,
+        name : "naman"
+    },
+    {
+        id : 3,
+        name : "raman"
+    },
+    {
+        id : 4,
+        name : "caman"
+    },
+    {
+        id : 5,
+        name : "daman"
+    },
+]
+
+
+console.log(newUser[1].id);
+
+
+console.log(Object.keys(newUser));       // doubt 
+
+console.log(Object.values(newUser));      // doubt 
+
+console.log(Object.entries(newUser));
+
+
+console.log(newUser.hasOwnProperty('0'));   // doubt 
+
+console.log(newUser.hasOwnProperty('email'));  
+
+
+
+const {a : first} = obj1
+console.log(first);  

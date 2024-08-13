b5dde34191936c0bbfe161e6cae410cc1b8f018d
manish-kumar-2002 <manishkumar8923245598@gmail.com>
2024-08-13 06:20:52 +0530

    add today self talk 4

--- 11-08-2004/function.js
+++ 11-08-2004/function.js
@@ -30,4 +30,55 @@ function getArrayElement(arrayValue){
     return arrayValue[1];
 }
 console.log(getArrayElement(myNewArray));
-;
+
+
+
+
+
+// this keyword
+
+
+
+const userDemo = {
+    id : 1,
+    name : "ram",
+    welcomeMessage : function (){
+        console.log(`hi ${this.name} welcome`);
+        console.log(this)
+    }
+}
+
+
+userDemo.welcomeMessage()
+userDemo.name = "manish"
+userDemo.welcomeMessage()
+
+
+
+
+
+
+function thisDemo() {
+    const name = "manish"
+    console.log(this.name);
+}
+thisDemo()
+
+console.log(this);
+
+
+
+
+// Arrow function
+
+
+ const arrowDemo = (num1,num2) => {
+    // console.log(this)
+    return num1 + num2
+}
+
+
+const arrowDemo2 = (num1,num2) =>  (num1 + num2)
+
+    console.log(arrowDemo(2,4))
+    console.log(arrowDemo2(2,4))
